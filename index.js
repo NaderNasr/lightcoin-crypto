7a926e2929221da108aa00c96c00a20f17125598
Nader Nasr <nadernasr7@gmail.com>
2022-01-21 20:42:16 -0500

    OOP transaction db

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    //New account === balance 0$
 
-class Withdrawal {
+    this.transactions = [];
+  }
 
-  constructor(amount) {
-    this.amount = amount;
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    this.account.addTransaction(this);
   }
 
 }
 
+class Deposit extends Transaction {
 
+  get value() {
+    return this.amount;
+  }
 
+}
 
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+let newUser = new Account('The-Anonymous-Cow');
+
+let t1;
+let t2;
+
+t1 = new Withdrawal(50.25, newUser);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, newUser);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t1 = new Deposit(1000.00, newUser);
+t1.commit();
+console.log('Deposit 1:', t1);
+console.log('Balance: ', newUser.balance);
